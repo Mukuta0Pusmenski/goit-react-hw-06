7c20cc96b41327929661610c73d2a84a8e455f86
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-02-20 23:20:52 +0200

    push=2/3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react-redux': 'node_modules/react-redux',
+      '@reduxjs/toolkit': 'node_modules/@reduxjs/toolkit',
+      'formik': 'node_modules/formik',
+      'yup': 'node_modules/yup'
+    }
+  },
   build: {
     rollupOptions: {
       external: ['formik', 'react-redux', 'yup', '@reduxjs/toolkit']
